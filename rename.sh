a062b7ae9a053a58b2d2816da3f349b4e559a751
David O <dev@Ds-iMac.local>
2023-12-02 12:13:47 -0800

    make corrections

--- rename.sh
+++ rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd html
 mv config-dist.php config.php
-mv adimin/config-dist.php admin/config.php
+mv admin/config-dist.php admin/config.php
 chmod 0777 config.php
 chmod 0777 admin/config.php
 
