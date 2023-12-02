7081d0f17b41a492b3ea76259adea02c6a2edf95
David O <dev@Ds-iMac.local>
2023-12-02 12:09:23 -0800

    editing

--- rename.sh
+++ rename.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-cd ..
-mv ecommerce_loader www
-rm -f LICENSE
 cd html
 mv config-dist.php config.php
 mv adimin/config-dist.php admin/config.php
-chmode 0777 config.php
-chmode 0777 admin/config.php
+chmod 0777 config.php
+chmod 0777 admin/config.php
 
 
 
