fa99910acf63942eb134fd8523c4d420cb237e29
David O <dev@Ds-iMac.local>
2023-12-02 11:50:23 -0800

    zsh:1: command not found: wq

--- rename.sh
+++ rename.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+cd ..
+mv ecommerce_loader www
+rm -f LICENSE
+cd html
+mv config-dist.php config.php
+mv adimin/config-dist.php admin/config.php
+chmode 0777 config.php
+chmode 0777 admin/config.php
+
 
 
